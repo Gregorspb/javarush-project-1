977d4962a1740bdf8320a9f0e905abc4fc27cbda
Gregorspb <bogomolovspb@gmail.com>
2022-05-31 15:31:40 +0300

    Add exit

--- src/ru/javarush/gbogomolov/module1/CaesarCipher.java
+++ src/ru/javarush/gbogomolov/module1/CaesarCipher.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 
 
-public class Main {
+public class CaesarCipher {
 
     private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з',
             'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
